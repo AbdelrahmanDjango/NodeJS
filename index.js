d530214923e9de49773ba06656e3c00e1532a826
Abdelrahman Mahmoud <151494963+AbdelrahmanDjango@users.noreply.github.com>
2024-02-21 08:38:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express');
 const members = require('./members.js');
 const { Console } = require('console');
 const app = express();
+// In this line I make logger middleware function globaly, working with every req.
 app.use(logger);
 
 app.get('/api/members/', (req,res) => {
     res.json(members);
 });
 
-app.get('/api/members/:id', (req, res) => {
+// But this line, using logger in specify function.
+app.get('/api/members/:id', logger, (req, res) => {
     res.json(members.filter(member => member.id === parseInt(req.params.id)));
 });
 
